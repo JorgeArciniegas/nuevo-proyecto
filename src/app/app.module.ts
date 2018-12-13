96e28c16bfbc3ef46caa3d7af4aa14452187b2d3
giuseppe <g.pec79@gmail.com>
2018-12-13 16:03:58 +0100

    flexlayout base

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,13 +1,13 @@
 import { NgModule } from '@angular/core';
+import { FlexLayoutModule } from '@angular/flex-layout';
 import { BrowserModule } from '@angular/platform-browser';
 import { AppComponent } from './app.component';
 import { routing } from './app.routing';
 import { AppSettings } from './app.settings';
 import { ProductsComponent } from './products/products.component';
-
 @NgModule({
   declarations: [AppComponent, ProductsComponent],
-  imports: [BrowserModule, routing],
+  imports: [BrowserModule, FlexLayoutModule, routing],
   providers: [AppSettings],
   bootstrap: [AppComponent]
 })
