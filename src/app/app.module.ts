99b905f7d08270265822be0617bae93c2b9d17ce
Nino Marino <a.marino@odissea.at>
2018-12-13 16:11:07 +0100

    Header

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,11 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppComponent } from './app.component';
 import { routing } from './app.routing';
 import { AppSettings } from './app.settings';
+import { HeaderComponent } from './component/header/header.component';
 import { ProductsComponent } from './products/products.component';
 
 @NgModule({
-  declarations: [AppComponent, ProductsComponent],
+  declarations: [AppComponent, ProductsComponent, HeaderComponent],
   imports: [BrowserModule, routing],
   providers: [AppSettings],
   bootstrap: [AppComponent]
