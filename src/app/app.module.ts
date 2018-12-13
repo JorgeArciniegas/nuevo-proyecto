5322ea97fcfd8b473f615e4fbffb5c01b55953b0
giuseppe <g.pec79@gmail.com>
2018-12-13 14:21:49 +0100

    created container components

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,9 +2,10 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { AppComponent } from './app.component';
 import { AppSettings } from './app.settings';
+import { ProductsComponent } from './products/products.component';
 
 @NgModule({
-  declarations: [AppComponent],
+  declarations: [AppComponent, ProductsComponent],
   imports: [BrowserModule],
   providers: [AppSettings],
   bootstrap: [AppComponent]
