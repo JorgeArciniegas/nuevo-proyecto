95429aea386a0d2295ccbfe83e4b3a5b3395b494
Nino Marino <a.marino@odissea.at>
2018-12-17 15:39:52 +0100

    #18445 Added icon size calculation

--- src/app/component/header/application-menu/application-menu.component.ts
+++ src/app/component/header/application-menu/application-menu.component.ts
@@ -1,5 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { AppSettings } from 'src/app/app.settings';
+import { ProductsService } from 'src/app/products/products.service';
+import { IconSize } from '../iconSize.model';
 
 @Component({
   selector: 'app-application-menu',
@@ -8,10 +10,23 @@ import { AppSettings } from 'src/app/app.settings';
 })
 export class ApplicationMenuComponent implements OnInit {
   public settings: AppSettings;
+  public logoIcon: IconSize;
+  public menuIcon: IconSize;
+  public buttonIcon: IconSize;
 
-  constructor(public readonly appSettings: AppSettings) {
+  constructor(
+    public readonly appSettings: AppSettings,
+    public productService: ProductsService
+  ) {
     this.settings = appSettings;
   }
 
-  ngOnInit() {}
+  ngOnInit() {
+    const barHeight =
+      this.productService.windowSize.height -
+      this.productService.windowSize.columnHeight;
+    this.logoIcon = new IconSize(barHeight * 0.9, barHeight * 0.9);
+    this.menuIcon = new IconSize(barHeight * 0.9, barHeight * 0.9);
+    this.buttonIcon = new IconSize(barHeight * 0.7, barHeight * 0.7);
+  }
 }
---
+++ src/app/component/header/iconSize.model.ts
@@ -0,0 +1,8 @@
+export class IconSize {
+  constructor(width: number, height?: number) {
+    this.width = width;
+    this.height = height;
+  }
+  width: number;
+  height: number;
+}
