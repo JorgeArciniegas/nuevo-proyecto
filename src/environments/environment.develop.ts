67dc7dbdbdb7bad4f841df986b63b7fc03b984c3
Nino Marino <a.marino@odissea.at>
2018-12-19 10:41:06 +0100

    #18445 Added icons

--- src/environments/environment.develop.ts
+++ src/environments/environment.develop.ts
@@ -11,28 +11,68 @@ export const environment: Environment = {
       defaultAmount: [1, 2, 5, 10],
       toolbarButton: {
         name: 'dogracing',
-        icon: 'icon',
+        icon: 'Dog',
         route: 'products/dogracing'
       }
     },
     {
-      name: 'hourseracing',
-      label: 'Hourse Racing',
+      name: 'footballita',
+      label: 'Football Ita',
       defaultAmount: [1, 2, 5, 10],
       toolbarButton: {
-        name: 'hourseracing',
-        icon: 'icon',
-        route: 'products/hourseracing'
+        name: 'footballita',
+        icon: 'Soccer-ita',
+        route: 'products/soccerita'
       }
     },
     {
-      name: 'soccerit',
-      label: 'Soccer IT',
+      name: 'footballeng',
+      label: 'Football Eng',
       defaultAmount: [1, 2, 5, 10],
       toolbarButton: {
-        name: 'soccerit',
-        icon: 'icon',
-        route: 'products/soccerit'
+        name: 'footballeng',
+        icon: 'Soccer-eng',
+        route: 'products/soccereng'
+      }
+    },
+    {
+      name: 'roulette',
+      label: 'American Roulette',
+      defaultAmount: [1, 2, 5, 10],
+      toolbarButton: {
+        name: 'roulette',
+        icon: 'Roulette',
+        route: 'products/roulette'
+      }
+    },
+    {
+      name: 'horseracing',
+      label: 'Horse Racing',
+      defaultAmount: [1, 2, 5, 10],
+      toolbarButton: {
+        name: 'horseracing',
+        icon: 'Horse',
+        route: 'products/horseracing'
+      }
+    },
+    {
+      name: 'cockfight',
+      label: 'Cock Fight',
+      defaultAmount: [1, 2, 5, 10],
+      toolbarButton: {
+        name: 'cockfight',
+        icon: 'Galli',
+        route: 'products/cockfight'
+      }
+    },
+    {
+      name: 'keno',
+      label: 'keno',
+      defaultAmount: [1, 2, 5, 10],
+      toolbarButton: {
+        name: 'keno',
+        icon: 'Bingo',
+        route: 'products/keno'
       }
     }
   ]
