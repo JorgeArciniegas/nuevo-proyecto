7c14a6055bf2073582ba1749d71fe809d7a9b9cc
Nino Marino <a.marino@odissea.at>
2019-01-10 16:11:12 +0100

    url

--- src/app/app.mappingurl.ts
+++ src/app/app.mappingurl.ts
@@ -1,5 +1,2 @@
 // if the api need to Authorization Header, put on here the url or part its
-export const MappingUrls: string[] = [
-  'api/virtual/lastresults',
-  'api/virtual/countdown'
-];
+export const MappingUrls: string[] = ['api/virtual/lastresults'];
