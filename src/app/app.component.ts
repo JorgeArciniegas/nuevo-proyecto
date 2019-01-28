a6762f75fc992d4fdb808131584eebea8813f223
Nino Marino <a.marino@odissea.at>
2019-01-28 15:22:13 +0100

    remove unused code

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,4 @@
-import { Component, HostListener } from '@angular/core';
+import { Component } from '@angular/core';
 import { AppSettings } from './app.settings';
 import { Settings } from './app.settings.model';
 
@@ -13,51 +13,4 @@ export class AppComponent {
   constructor(public readonly appSettings: AppSettings) {
     this.settings = appSettings;
   }
-
-  // @HostListener('document:click', ['$event'])
-  // clickout(event) {
-  //   if(this.eRef.nativeElement.contains(event.target)) {
-  //     this.text = "clicked inside";
-  //   } else {
-  //     this.text = "clicked outside";
-  //   }
-  // }
-
-  // (function() {
-  //   var hidden = "hidden";
-  
-  //   // Standards:
-  //   if (hidden in document)
-  //     document.addEventListener("visibilitychange", onchange);
-  //   else if ((hidden = "mozHidden") in document)
-  //     document.addEventListener("mozvisibilitychange", onchange);
-  //   else if ((hidden = "webkitHidden") in document)
-  //     document.addEventListener("webkitvisibilitychange", onchange);
-  //   else if ((hidden = "msHidden") in document)
-  //     document.addEventListener("msvisibilitychange", onchange);
-  //   // IE 9 and lower:
-  //   else if ("onfocusin" in document)
-  //     document.onfocusin = document.onfocusout = onchange;
-  //   // All others:
-  //   else
-  //     window.onpageshow = window.onpagehide
-  //     = window.onfocus = window.onblur = onchange;
-  
-  //   function onchange (evt) {
-  //     var v = "visible", h = "hidden",
-  //         evtMap = {
-  //           focus:v, focusin:v, pageshow:v, blur:h, focusout:h, pagehide:h
-  //         };
-  
-  //     evt = evt || window.event;
-  //     if (evt.type in evtMap)
-  //       document.body.className = evtMap[evt.type];
-  //     else
-  //       document.body.className = this[hidden] ? "hidden" : "visible";
-  //   }
-  
-  //   // set the initial state (but only if browser supports the Page Visibility API)
-  //   if( document[hidden] !== undefined )
-  //     onchange({type: document[hidden] ? "blur" : "focus"});
-  // })();
 }
