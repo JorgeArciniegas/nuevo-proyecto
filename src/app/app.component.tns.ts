5a1b6e5e2ebbbdd7e2a251c9ebab19a3e15c5273
Nino Marino <a.marino@odissea.at>
2019-02-07 12:41:27 +0100

    last chages

--- src/app/app.component.tns.ts
+++ src/app/app.component.tns.ts
@@ -1,4 +1,8 @@
 import { Component } from '@angular/core';
+import {
+  connectionType,
+  getConnectionType
+} from 'tns-core-modules/connectivity';
 import { AppSettings } from './app.settings';
 import { Settings } from './app.settings.model';
 
@@ -9,8 +13,12 @@ import { Settings } from './app.settings.model';
 })
 export class AppComponent {
   settings: Settings;
+  public connectionAviable: boolean;
 
   constructor(public readonly appSettings: AppSettings) {
     this.settings = appSettings;
+
+    console.log(getConnectionType());
+    this.connectionAviable = getConnectionType() !== connectionType.none;
   }
 }
