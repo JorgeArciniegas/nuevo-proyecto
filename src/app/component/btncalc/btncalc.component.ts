a3d809ca53b027969810bd33b1becd4e306303e9
unknown <olea.oswaldo@gmail.com>
2019-05-03 08:53:59 +0200

    #21070 preset stakes empty overlay blocked lock template html

--- src/app/component/btncalc/btncalc.component.ts
+++ src/app/component/btncalc/btncalc.component.ts
@@ -4,26 +4,39 @@ import { ProductsService } from '../../products/products.service';
 import { CouponService } from '../coupon/coupon.service';
 import { BtncalcComponentCommon } from './btncalc.component.common';
 import { BtncalcService } from './btncalc.service';
+import { Subscription } from 'rxjs';
+import { MediaChange, MediaObserver } from '@angular/flex-layout';
 
 @Component({
   selector: 'app-btncalc',
   templateUrl: './btncalc.component.html',
   styleUrls: ['./btncalc.component.scss']
 })
-export class BtncalcComponent extends BtncalcComponentCommon implements OnInit, OnDestroy {
-
+export class BtncalcComponent extends BtncalcComponentCommon
+  implements OnInit, OnDestroy {
+  observableMediaSubscribe: Subscription;
   @Input()
   rowHeight: number;
   constructor(
+    private observableMedia: MediaObserver,
     productService: ProductsService,
     btncalcService: BtncalcService,
     appSetting: AppSettings,
-    couponService: CouponService
+    couponService: CouponService,
+    public service: ProductsService
   ) {
     super(productService, btncalcService, appSetting, couponService);
+
+    this.observableMediaSubscribe = this.observableMedia.media$.subscribe(
+      (change: MediaChange) => {
+        this.service.breakpoint = this.service.gridByBreakpoint[change.mqAlias];
+        this.service.fnWindowsSize();
+        this.rowHeight = (this.service.windowSize.columnHeight - 30) / 11;
+      }
+    );
   }
 
-  ngOnInit(): void { }
+  ngOnInit(): void {}
 
   ngOnDestroy(): void {
     this.polyfunctionalValueSubscribe.unsubscribe();
