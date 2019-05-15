09b99ecaedb0bb34ddd41cc5100f7a224b95b241
Nino <a.marino@odissea.at>
2019-05-15 16:39:19 +0200

    login nativescript

--- src/app/app.component.tns.ts
+++ src/app/app.component.tns.ts
@@ -2,6 +2,7 @@ import { Component } from '@angular/core';
 import { connectionType, getConnectionType } from 'tns-core-modules/connectivity';
 import { AppSettings } from './app.settings';
 import { Settings } from './app.settings.model';
+import { UserService } from './services/user.service';
 
 @Component({
   selector: 'app-root',
@@ -12,7 +13,10 @@ export class AppComponent {
   settings: Settings;
   public connectionAviable: boolean;
 
-  constructor(public readonly appSettings: AppSettings) {
+  constructor(
+    public readonly appSettings: AppSettings,
+    public userService: UserService
+  ) {
     this.settings = appSettings;
 
     this.connectionAviable = getConnectionType() !== connectionType.none;
