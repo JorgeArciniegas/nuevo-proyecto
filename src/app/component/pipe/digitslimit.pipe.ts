be833157de04deb727c2fcb3df0f0ad1cdb08201
giuseppe <g.pec79@gmail.com>
2019-06-14 16:41:27 +0200

    21766 merge develop and improvement Digitslimits

--- src/app/component/pipe/digitslimit.pipe.ts
+++ src/app/component/pipe/digitslimit.pipe.ts
@@ -8,12 +8,18 @@ export class DigitslimitPipe implements PipeTransform {
     const valuePreset = value.toString();
     if (!value) {
       return null;
-    } else if (value > 999 && value < 999999) {
+    } else if (value > 999 && value < 9999) {
       return valuePreset.substr(0, 1) + 'K';
-    } else if (value > 999999) {
+    } else if (value > 9999 && value < 99999) {
+      return valuePreset.substr(0, 2) + 'K';
+    }  else if (value > 99999 && value < 999999) {
+      return valuePreset.substr(0, 3) + 'K';
+    } else if (value > 999999 && value < 9999999 ) {
       return valuePreset.substr(0, 1) + 'M';
-      /* } else if (value > 4) {
-      return valuePreset.substr(0, 1) + 'M'; */
+    } else if (value > 9999999 && value < 99999999 ) {
+        return valuePreset.substr(0, 2) + 'M';
+    } else if (value > 99999999 && value < 999999999 ) {
+        return valuePreset.substr(0, 3) + 'M';
     } else {
       return value;
     }
