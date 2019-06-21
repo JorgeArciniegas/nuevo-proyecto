fb6ffb99a29695dcd252e56e79d065e1e85f0410
giuseppe <g.pec79@gmail.com>
2019-06-21 15:18:52 +0200

    #22288 removed the comments

--- src/app/products/main/main.module.ts
+++ src/app/products/main/main.module.ts
@@ -1,11 +1,12 @@
 import { CommonModule } from '@angular/common';
-import { NgModule } from '@angular/core';
+import { NgModule, CUSTOM_ELEMENTS_SCHEMA } from '@angular/core';
 import { RouterModule } from '@angular/router';
 import { SharedModule } from '../../shared/shared.module';
 import { componentDeclarations, routes } from './main.common';
 
 @NgModule({
-  declarations: [componentDeclarations ],
-  imports: [CommonModule, SharedModule, RouterModule.forChild(routes)]
+  declarations: [componentDeclarations],
+  imports: [CommonModule, SharedModule, RouterModule.forChild(routes)],
+  schemas: [CUSTOM_ELEMENTS_SCHEMA]
 })
 export class MainModule { }
--- src/app/products/main/playable-board/playable-board.component.ts
+++ src/app/products/main/playable-board/playable-board.component.ts
@@ -9,55 +9,14 @@ import { SpecialBet, TypePlacingRace } from '../main.models';
   templateUrl: './playable-board.component.html',
   styleUrls: ['./playable-board.component.scss']
 })
-export class PlayableBoardComponent implements OnInit, OnDestroy {
+export class PlayableBoardComponent {
   @Input()
   public rowHeight: number;
   @Input()
   public show?: boolean;
-
-  public specialBet: typeof SpecialBet = SpecialBet;
-  private currentRaceSubscription: Subscription;
-
-  public TypePlacingRace = TypePlacingRace;
-  public playableTitle: string[];
   public typeLayout: typeof LAYOUT_RESULT_LIST_TYPE = LAYOUT_RESULT_LIST_TYPE;
+
+
   constructor(public productService: ProductsService) { }
 
-  ngOnInit() {
-   /*  this.currentRaceSubscription = this.service.currentEventObserve.subscribe(
-      raceIndex =>
-        (this.service.placingRace.raceNumber = this.service.raceDetails.races[
-          raceIndex
-        ].number)
-    ); */
-  }
-
-  ngOnDestroy(): void {
-    // this.currentRaceSubscription.unsubscribe();
-  }
-  /*
-  dogplaced(dog: Dog): void {
-    this.service.placingOdd(dog);
-  }
-
-  specialBets(type: string): void {
-    if (this.service.placingRace.dogs.length > 0) {
-      this.service.resetPlayRacing();
-    }
-    if (
-      this.service.placingRace.isSpecialBets &&
-      this.specialBet[type] === this.service.placingRace.specialBetValue
-    ) {
-      this.service.placingRace.isSpecialBets = false;
-      this.service.placingRace.specialBetValue = null;
-    } else {
-      this.service.placingRace.isSpecialBets = true;
-      this.service.placingRace.specialBetValue = this.specialBet[type];
-    }
-    this.service.placeOdd();
-  }
-
-  setRepeat(repeat: number): void {
-    this.service.placingRace.repeat = repeat;
-  } */
 }
