941e763b10ec4d1805aee88125d18163874b9c6e
Leonardo <lc@hiho.it>
2019-06-27 17:04:32 +0200

    #22379

--- src/app/products/main/event-control/event-control.component.ts
+++ src/app/products/main/event-control/event-control.component.ts
@@ -1,8 +1,6 @@
-import { Component, OnDestroy, OnInit } from '@angular/core';
+import { Component, OnDestroy } from '@angular/core';
 import { EventControlService } from './event-control.service';
 import { EventControl } from './event-control.model';
-import { MainService } from '../main.service';
-import { Subscription } from 'rxjs';
 import { ProductsService } from '../../products.service';
 
 @Component({
@@ -10,30 +8,14 @@ import { ProductsService } from '../../products.service';
   templateUrl: './event-control.component.html',
   styleUrls: ['./event-control.component.scss']
 })
-export class EventControlComponent implements OnInit, OnDestroy {
+export class EventControlComponent implements OnDestroy {
   public eventControlDetails: EventControl;
   //  private currentEventSubscription: Subscription;
   constructor(
     private eventControlService: EventControlService,
-    private mainService: MainService,
     public productService: ProductsService
-  ) {
-    // this.currentEventSubscription = this.mainService.currentEventSubscribe.subscribe(
-    //   event => {
-    //     /**
-    //      * @eventsDetails is passed as input to a control template
-    //      */
-    //     this.eventControlDetails = this.eventControlService.getEventControl();
-    //     (this.eventControlDetails.eventTimeMinutes = this.mainService.raceDetails.raceTime.minute),
-    //       (this.eventControlDetails.eventTimeSeconds = this.mainService.raceDetails.raceTime.second);
-    //     console.log(this.eventControlDetails);
-    //   }
-    // );
-  }
-
-  ngOnInit() {}
+  ) {}
   ngOnDestroy() {
-    // this.currentEventSubscription.unsubscribe();
     this.eventControlService.customUnsubscribe();
   }
 }
--- src/app/products/main/events-list/events-list.component.ts
+++ src/app/products/main/events-list/events-list.component.ts
@@ -1,9 +1,8 @@
 import { Component, Input, OnInit, OnDestroy } from '@angular/core';
 import { EventsListService } from './events-list.service';
 import { EventsList } from './event-list.model';
-import { MainService } from '../main.service';
 import { ProductsService } from '../../products.service';
-import { Subscription } from 'rxjs';
+
 @Component({
   selector: 'app-events-list',
   templateUrl: './events-list.component.html',
@@ -16,9 +15,7 @@ export class EventListComponent implements OnInit, OnDestroy {
   public show?: boolean;
   public eventsDetails: EventsList;
   public nativeNextEventsItems: string;
-  //  private currentEventSubscription: Subscription;
   constructor(
-    private mainService: MainService,
     private eventService: EventsListService,
     private productService: ProductsService
   ) {}
@@ -34,15 +31,6 @@ export class EventListComponent implements OnInit, OnDestroy {
         this.productService.product.layoutProducts.nextEventItems
       );
     });
-    // subscribe to the event change
-    // this.currentEventSubscription = this.mainService.currentEventSubscribe.subscribe(
-    //   event => {
-    //     /**
-    //      * @eventsDetails is passed as input to a list template
-    //      */
-    //     this.eventsDetails = this.eventService.getEventDetailsList();
-    //   }
-    // );
   }
   ngOnDestroy() {
     this.eventService.customUnsubscribe();
