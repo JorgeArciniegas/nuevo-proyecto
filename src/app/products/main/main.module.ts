f80cf03dc8e884058ad6cefc8e7b383147b4f4e8
giuseppe <g.pec79@gmail.com>
2019-07-08 17:17:45 +0200

    #21826 aggiunta icona

--- src/app/products/main/main.module.ts
+++ src/app/products/main/main.module.ts
@@ -1,9 +1,8 @@
 import { CommonModule } from '@angular/common';
-import { NgModule, CUSTOM_ELEMENTS_SCHEMA } from '@angular/core';
+import { CUSTOM_ELEMENTS_SCHEMA, NgModule } from '@angular/core';
 import { RouterModule } from '@angular/router';
 import { SharedModule } from '../../shared/shared.module';
 import { componentDeclarations, routes } from './main.common';
-import { CockfightComponent } from './results/templates/cockfight/cockfight.component';
 @NgModule({
   declarations: [componentDeclarations],
   imports: [CommonModule, SharedModule, RouterModule.forChild(routes)],
