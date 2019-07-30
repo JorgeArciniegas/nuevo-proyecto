60346490e61444203f278eb436fde38a21744538
Stefano Diquattro <s.diquattro@odissea.at>
2019-07-30 18:27:55 +0200

    #22962 - Work in progress.

--- src/app/products/main/playable-board/templates/soccer/soccer.component.ts
+++ src/app/products/main/playable-board/templates/soccer/soccer.component.ts
@@ -1,8 +1,10 @@
 import { Component, OnDestroy, OnInit, Input } from '@angular/core';
-import { VirtualBetTournament } from '@elys/elys-api';
+import { VirtualBetSelection } from '@elys/elys-api';
 import { Subscription, timer } from 'rxjs';
 import { MainService } from '../../../main.service';
 import { VirtualBetTournamentExtended, Match } from '../../../main.models';
+import { PolyfunctionalArea } from 'src/app/products/products.model';
+import { ProductsService } from 'src/app/products/products.service';
 
 @Component({
   selector: 'app-playable-board-soccer',
@@ -15,28 +17,42 @@ export class SoccerComponent implements OnInit, OnDestroy {
   private currentEventSubscription: Subscription;
   public tournament: VirtualBetTournamentExtended;
   public selectedMatch: Match;
+  public oddsSelected: number[];
+  private polyfunctionalAreaSubscription: Subscription;
 
-  constructor(private mainService: MainService) {
+  constructor(private mainService: MainService, private productService: ProductsService) {
+    this.oddsSelected = [];
     // Get the event's detail at the access of the section
     this.getTournamentDetails();
+  }
 
+  ngOnInit() {
     // Get the event's details.
     this.currentEventSubscription = this.mainService.currentEventObserve.subscribe(() => {
       this.getTournamentDetails();
     });
-  }
 
-  ngOnInit() {}
+    // Get the change of the polyfunctional area's object.
+    this.polyfunctionalAreaSubscription = this.productService.polyfunctionalAreaObservable.subscribe(polyfunctional => {
+      // Delete the list of selections when the object of polyfunctional area is empty.
+      if (polyfunctional.odds.length === 0 && this.oddsSelected.length !== 0) {
+        this.oddsSelected = [];
+      } else {
+        this.checkOddSelected(polyfunctional);
+      }
+    });
+  }
 
   ngOnDestroy() {
     this.currentEventSubscription.unsubscribe();
+    this.polyfunctionalAreaSubscription.unsubscribe();
   }
 
   /**
    * Method to get the details of the currently selected tournament (week).
    * @param attemptsNumber The number of attempted call executed. Limit of attempts is 5 recall.
    */
-  getTournamentDetails(attemptsNumber: number = 0) {
+  getTournamentDetails(attemptsNumber: number = 0): void {
     this.mainService
       .getCurrentTournament()
       .then(tournamentDetails => {
@@ -50,5 +66,28 @@ export class SoccerComponent implements OnInit, OnDestroy {
           console.log(error);
         }
       });
+  }
+
+  selectOdd(marketId: number, selection: VirtualBetSelection): void {
+    const index = this.oddsSelected.indexOf(selection.id);
+    // Insert or delete the selection from the list.
+    if (index === -1) {
+      this.oddsSelected.push(selection.id);
+    } else {
+      this.oddsSelected.splice(index, 1);
+    }
+    this.mainService.placingOddByOdd(marketId, selection);
+  }
+
+  /**
+   * When "oddsSelected" does not have the odds contains from "polifunctionalArea", append it.
+   * @param polyfunctional PolyfunctionalArea
+   */
+  private checkOddSelected(polyfunctional: PolyfunctionalArea): void {
+    polyfunctional.odds.filter(item => {
+      if (!this.oddsSelected.includes(item.id)) {
+        this.oddsSelected.push(item.id);
+      }
+    });
   }
 }
