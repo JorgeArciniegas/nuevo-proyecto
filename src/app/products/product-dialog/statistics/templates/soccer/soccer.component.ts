bef0e3051e176e5295b4b3230c484301f4ad8b3f
nick <n.denny@odissea.at>
2019-08-29 16:17:41 +0200

    aggiunto gestione paginazione statistiche soccer web
    #23277

--- src/app/products/product-dialog/statistics/templates/soccer/soccer.component.ts
+++ src/app/products/product-dialog/statistics/templates/soccer/soccer.component.ts
@@ -1,8 +1,8 @@
 import { Component, OnInit, Input } from '@angular/core';
-import { DataStaticsChart } from '../soccer/soccer.model';
 import { DialogService } from '../../../../../products/dialog.service';
 import { BetDataDialog } from '../../../../../products/products.model';
 import { AppSettings } from '../../../../../app.settings';
+import { VirtualBetCompetitor } from '@elys/elys-api/lib/virtual/virtual.models';
 
 @Component({
   selector: 'app-statistics-soccer',
@@ -12,23 +12,58 @@ import { AppSettings } from '../../../../../app.settings';
 export class SoccerComponent implements OnInit {
   @Input()
   data: BetDataDialog;
+  pageNumber: number;
+  currentPage: number;
+  readonly elementsPerPage = 10;
+  showingCompetitors: VirtualBetCompetitor[] = [];
 
-  dataStaticsChart: DataStaticsChart;
   constructor(
     private dialog: DialogService,
     public readonly appSetting: AppSettings
   ) {
-    this.dataStaticsChart = new DataStaticsChart();
+    this.currentPage = 1;
   }
 
   ngOnInit() {
     this.data.statistics.virtualBetCompetitor.sort((a, b) =>
       a.ito <= b.ito ? -1 : 1
     );
-    this.data.statistics.virtualBetCompetitor.map(item => {
-      this.dataStaticsChart.CONDITION += item.ff;
-      this.dataStaticsChart.ATTACK += item.ac[0];
-      this.dataStaticsChart.DEFENCE += item.ac[1];
-    });
+    this.showingCompetitors = this.data.statistics.virtualBetCompetitor.slice(
+      0,
+      10
+    );
+    this.pageNumber =
+      this.data.statistics.virtualBetCompetitor.length / this.elementsPerPage;
+    this.currentPage = 1;
+  }
+
+  previousPage(): void {
+    if (this.currentPage >= 1) {
+      --this.currentPage;
+      const start = (this.currentPage - 1) * this.elementsPerPage;
+      let end = this.currentPage * this.elementsPerPage;
+      if (end > this.data.statistics.virtualBetCompetitor.length) {
+        end = this.data.statistics.virtualBetCompetitor.length;
+      }
+      this.showingCompetitors = this.data.statistics.virtualBetCompetitor.slice(
+        start,
+        end
+      );
+    }
+  }
+
+  nextPage(): void {
+    if (this.currentPage < this.pageNumber) {
+      ++this.currentPage;
+      const start = (this.currentPage - 1) * this.elementsPerPage;
+      let end = this.currentPage * this.elementsPerPage;
+      if (end > this.data.statistics.virtualBetCompetitor.length) {
+        end = this.data.statistics.virtualBetCompetitor.length;
+      }
+      this.showingCompetitors = this.data.statistics.virtualBetCompetitor.slice(
+        start,
+        end
+      );
+    }
   }
 }
