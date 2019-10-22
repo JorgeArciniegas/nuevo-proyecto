d68b9c27fb5883e9baf393bb0e04a7e53a297a61
nick <n.denny@odissea.at>
2019-10-22 10:48:46 +0200

    Correzione gestione last results per prodotto soccer
    #24753

--- src/app/products/main/results/results.service.ts
+++ src/app/products/main/results/results.service.ts
@@ -33,52 +33,55 @@ export class ResultsService {
     this.listResult = [];
     this.elysApi.virtual.getLastResult(request)
       .then((eventResults: VirtualSportLastResultsResponse) => {
-        for (let i = 0; i < this.productService.product.layoutProducts.resultItems; i++) {
-          // results for products
-          let results: string[] = [];
-          // set the default parameters on the temporary EventResult
-          let tempEventResult: EventResult = {
-            eventLabel: eventResults.EventResults[i].EventName,
-            eventNumber: eventResults.EventResults[i].EventId
+        // results for products
+        if (this.productService.product.layoutProducts.type !== LAYOUT_TYPE.SOCCER) {
+          for (let i = 0; i < this.productService.product.layoutProducts.resultItems; i++) {
+            // results for products
+            let results: string[] = [];
+            // set the default parameters on the temporary EventResult
+            const tempEventResult: EventResult = {
+              eventLabel: eventResults.EventResults[i].EventName,
+              eventNumber: eventResults.EventResults[i].EventId
+            };
+
+            // finds and set sport's result
+            switch (this.productService.product.layoutProducts.type) {
+              case LAYOUT_TYPE.RACING:
+                results = eventResults.EventResults[i].Result.split('-');
+                tempEventResult.racePodium = {
+                  firstPlace: Number.parseInt(results[0]),
+                  secondPlace: Number.parseInt(results[1]),
+                  thirdPlace: Number.parseInt(results[2])
+                };
+                break;
+              case LAYOUT_TYPE.COCK_FIGHT:
+                results = eventResults.EventResults[i].Result.split('-');
+                tempEventResult.cockResult = {
+                  winner: Number.parseInt(results[0]),
+                  ou: results[1] as OVER_UNDER_COCKFIGHT,
+                  sector: Number.parseInt(results[2]),
+                };
+                break;
+              case LAYOUT_TYPE.KENO:
+                results = eventResults.EventResults[i].Result.split(',');
+                const kenoResults: number[] = results.map(result => Number.parseInt(result));
+                tempEventResult.kenoResults = kenoResults;
+                break;
+              default:
+                break;
+            }
+            // put the new object on the global list results
+            this.listResult.push(tempEventResult);
+          }
+        } else {
+          // create last Result
+          const tempEventResult: EventResult = {
+            eventLabel: eventResults.EventResults[0].TournamentName,
+            eventNumber: eventResults.EventResults[0].TournamentId
           };
 
-          // finds and set sport's result
-          switch (this.productService.product.layoutProducts.type) {
-            case LAYOUT_TYPE.RACING:
-              results = eventResults.EventResults[i].Result.split('-');
-              tempEventResult.racePodium = {
-                firstPlace: Number.parseInt(results[0]),
-                secondPlace: Number.parseInt(results[1]),
-                thirdPlace: Number.parseInt(results[2])
-              };
-              break;
-            case LAYOUT_TYPE.COCK_FIGHT:
-              results = eventResults.EventResults[i].Result.split('-');
-              tempEventResult.cockResult = {
-                winner: Number.parseInt(results[0]),
-                ou: results[1] as OVER_UNDER_COCKFIGHT,
-                sector: Number.parseInt(results[2]),
-              };
-              break;
-            case LAYOUT_TYPE.KENO:
-              results = eventResults.EventResults[i].Result.split(',');
-              const kenoResults: number[] = results.map(result => Number.parseInt(result));
-              tempEventResult.kenoResults = kenoResults;
-              break;
-            case LAYOUT_TYPE.SOCCER:
-              // assign last Result
-              tempEventResult = {
-                eventLabel: eventResults.EventResults[0].TournamentName,
-                eventNumber: eventResults.EventResults[0].TournamentId
-              };
-              // group by last Tournament
-              tempEventResult.soccerResult = eventResults.EventResults.filter(item => item.TournamentId === tempEventResult.eventNumber);
-              break;
-            default:
-              break;
-
-          }
-          // put the new object on the global list results
+          // group by last Tournament
+          tempEventResult.soccerResult = eventResults.EventResults.filter(item => item.TournamentId === tempEventResult.eventNumber);
           this.listResult.push(tempEventResult);
         }
       });
