24a31d24c340bc425096e36d3fa11c826526b464
nick <n.denny@odissea.at>
2019-10-23 11:09:11 +0200

    Creato template playable-board per prodotto keno
    #24752

---
+++ src/app/products/main/playable-board/templates/keno/keno-number.ts
@@ -0,0 +1,4 @@
+export interface KenoNumber {
+  number: number;
+  isSelected: boolean;
+}
---
+++ src/app/products/main/playable-board/templates/keno/keno.component.ts
@@ -0,0 +1,35 @@
+import { Component, OnInit, Input } from '@angular/core';
+import { KenoNumber } from './keno-number';
+
+@Component({
+  selector: 'app-playable-board-keno',
+  templateUrl: './keno.component.html',
+  styleUrls: ['./keno.component.scss']
+})
+export class KenoComponent implements OnInit {
+  @Input()
+  public rowHeight: number;
+  kenoTable: KenoNumber[] = [];
+
+  constructor() { }
+
+  ngOnInit(): void {
+    this.initKenoNumbers();
+  }
+
+  onNumberClick(kenoNumber: KenoNumber): void {
+    kenoNumber.isSelected = !kenoNumber.isSelected;
+  }
+
+  private initKenoNumbers(): void {
+    const kenoNumbers: KenoNumber[] = [];
+    for (let i = 1; i <= 80; ++i) {
+      const kenoNumber: KenoNumber = {
+        number: i,
+        isSelected: false
+      };
+      kenoNumbers.push(kenoNumber);
+    }
+    this.kenoTable = kenoNumbers;
+  }
+}
