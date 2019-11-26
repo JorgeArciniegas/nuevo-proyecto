57f2d5a651ef980adcb0fc1efce009d851602655
nick <n.denny@odissea.at>
2019-11-26 10:46:10 +0100

    Corretto puntamenti API staging

--- src/environments/environment.shawis-staging.ts
+++ src/environments/environment.shawis-staging.ts
@@ -11,7 +11,7 @@ export const environment: Environment = {
   bookmakerDetails:
     'Lorem ipsum dolor sit amet,consectetur adipisicing elit, sed doeiusmod tempor incididunt ut labore etdolore magna aliqua.',
   license: LICENSE_TYPE.SHAWIS,
-  baseApiUrl: 'https://qacom-apishawis.ody-services.net',
+  baseApiUrl: 'https://vg-apishawis.vg-services.net',
   pageTitle: 'Shawis - VDesk Kiosk',
   theme: 'shawis',
   faviconPath: 'app/themes/skins/shawis/image/Logo-header.png',
