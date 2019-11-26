d5ba6085346049106d75d061b1474f4dea6a0708
nick <n.denny@odissea.at>
2019-11-26 18:10:46 +0100

    correzione link api produzione

--- src/environments/environment.vgen.ts
+++ src/environments/environment.vgen.ts
@@ -12,7 +12,7 @@ export const environment: Environment = {
   bookmakerDetails:
     'Lorem ipsum dolor sit amet,consectetur adipisicing elit, sed doeiusmod tempor incididunt ut labore etdolore magna aliqua.',
   license: LICENSE_TYPE.DEMO_LICENSE,
-  baseApiUrl: 'https://apidemo.vg-services.net',
+  baseApiUrl: 'https://vg-apidemo.odissea-services.net',
   pageTitle: 'VDESK-KIOSK',
   theme: 'develop',
   faviconPath: 'app/themes/skins/develop/image/Logo-header.png',
