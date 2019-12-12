452ece84faf3a2f3de34c6895cbecee412e0b9ee
giuseppe <g.pec79@gmail.com>
2019-12-12 15:44:02 +0100

    Updated error page #25722

--- src/app/error-page/error-page.component.ts
+++ src/app/error-page/error-page.component.ts
@@ -1,6 +1,5 @@
 import { Component } from '@angular/core';
 import { AppSettings } from '../app.settings';
-import { StorageService } from '../services/utility/storage/storage.service';
 import { RouterService } from '../services/utility/router/router.service';
 
 @Component({
@@ -9,14 +8,7 @@ import { RouterService } from '../services/utility/router/router.service';
   styleUrls: ['./error-page.component.scss']
 })
 export class ErrorPageComponent {
-
-  private _homeExternalLink: string;
-  public get homeExternalLink(): string {
-    const urlLink = this.storageService.getData('callBackURL');
-    return urlLink ? urlLink : '/';
-  }
-
-  constructor(private storageService: StorageService, public appSetting: AppSettings, private routerService: RouterService) { }
+  constructor(public appSetting: AppSettings, private routerService: RouterService) { }
 
   gotoHome() {
     if (this.appSetting.loginInteractive) {
