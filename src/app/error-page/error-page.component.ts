4f3ad47f6aee7ced39584a4d5723016260131c34
giuseppe <g.pec79@gmail.com>
2019-12-12 15:42:53 +0100

    Error page improvement callback to home. #25720

--- src/app/error-page/error-page.component.ts
+++ src/app/error-page/error-page.component.ts
@@ -1,4 +1,7 @@
 import { Component } from '@angular/core';
+import { AppSettings } from '../app.settings';
+import { StorageService } from '../services/utility/storage/storage.service';
+import { RouterService } from '../services/utility/router/router.service';
 
 @Component({
   selector: 'app-error-page',
@@ -6,5 +9,20 @@ import { Component } from '@angular/core';
   styleUrls: ['./error-page.component.scss']
 })
 export class ErrorPageComponent {
-  constructor() { }
+
+  private _homeExternalLink: string;
+  public get homeExternalLink(): string {
+    const urlLink = this.storageService.getData('callBackURL');
+    return urlLink ? urlLink : '/';
+  }
+
+  constructor(private storageService: StorageService, public appSetting: AppSettings, private routerService: RouterService) { }
+
+  gotoHome() {
+    if (this.appSetting.loginInteractive) {
+      this.routerService.getRouter().navigateByUrl('/');
+    } else {
+      this.routerService.callBackToBrand();
+    }
+  }
 }
