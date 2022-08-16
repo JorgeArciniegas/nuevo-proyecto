1d120f97177567059b763a052e7fb5dde588c8db
Diana Stasiv <diana.st@code-care.pro>
2022-08-16 16:47:40 +0300

    Add unit tests for router service

--- src/app/services/utility/storage/storage.service.spec.ts
+++ src/app/services/utility/storage/storage.service.spec.ts
@@ -1,40 +1,7 @@
 import { TestBed } from "@angular/core/testing";
 import { ElysStorageLibService } from "@elys/elys-storage-lib";
 import { StorageService } from "./storage.service";
-
-class ElysStorageLibServiceStub {
-  private elysStore: any = {};
-
-  getData(key: string): any {
-    return key in this.elysStore ? this.elysStore[key] : null;
-  }
-
-  setData(key: string, value: any): void {
-    this.elysStore[key] = value;
-  }
-
-  checkIsExist(key: string): boolean {
-    return key in this.elysStore;
-  }
-
-  destroy(): void {
-    this.elysStore = {};
-  }
-
-  removeItems(keys: string | string[]): void {
-    if(Array.isArray(keys)) {
-      keys.forEach(key => {
-        delete this.elysStore[key];
-      });
-    } else {
-      delete this.elysStore[keys];
-    }
-  }
-
-  checkDataIsValid(key: string): boolean {
-    return (typeof this.elysStore[key] === 'undefined' || this.elysStore[key] === null) ? false : true;
-  }
-}
+import { ElysStorageLibServiceStub } from '../../../mock/elys-storage.mock';
 
 describe('StorageService', () => {
   let service: StorageService;
